f95b9694d0fa4be5c6f53c0462e5cd8f0c7c458e
okimototakuya <okimoto.takuya.sm@tut.jp>
2022-03-04 00:54:36 +0900

    [*] とりあえずk_means.pyを実装したが、エラー。 - 22行目: 重心の初期値centroid[0]   # ValueError: setting an array element with a sequence.

---
+++ clustering/k_means.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+from sklearn import datasets
+
+def k_means(cluster_num, observed_data, max_iter):
+    '''
+    k-means法
+
+    Input
+    -----
+    - cluster_num: int型
+        機械学習ユーザが、仮定するクラスタ数。
+    - observed_data: numpy.ndarray型
+        # 観測データ。クラスタリングの対象になる。
+        # 行: 各観測データ
+        # データ: 各々のデータの特徴量
+    - max_iter: int型
+        最大反復回数
+    '''
+    feature_dim = len(observed_data[0])                             # 特徴量の次元
+    centroids = np.zeros(max_iter)                                  # 重心座標
+    centroids[0] = [np.random.uniform for i in range(feature_dim)]  # 重心の初期値
+    which_cluster = np.zeros(len(observed_data), dtype='int64')     # 各観測データの所属クラスタ
+    for i in range(max_iter):
+        for j in range(len(observed_data)):
+            div = [sum(np.square(observed_data[j] - centroids[i][k])) for k in range(cluster_num)]
+            which_cluster[j] = div.index(min(div))
+        for k_cluster in range(cluster_num):    # 重心の再計算
+            index_k_cluster = [i for i, x in enumerate(which_cluster) if x == k_cluster]  # クラスタkのインデックスを取得
+            centroids[i+1][k_cluster] = sum([observed_data[i] for i in index_k_cluster]) / len(index_k_cluster)
+        # 2022.3.3[HACK]: もしも重心が変わっていなければ、終了
+    return which_cluster
+
+
+def main():
+    '''
+    k-means法により、クラスタリングを実行する。
+    '''
+    # データのロード
+    iris = datasets.load_iris()
+    df_iris = pd.DataFrame(
+        data = iris.data,               # 各観測データの値 (座標) : 各特徴量の値
+        columns = iris.feature_names    # 各観測データのクラスタ値: 各特徴量の名称
+        )
+    cluster = k_means(3, df_iris.values, 1000)
+    print('クラスタ値: {cluster}'.format(cluster=cluster[:10]))
+
+if __name__ == '__main__':
+    main()
